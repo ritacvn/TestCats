4a1aa8725fcd7c48e5f296831506e79c7a074028
ritacvn <ritadecassia.vasconcelos@hotmail.com>
2025-02-09 20:05:14 -0400

    first commit

---
+++ CatsTest/CatsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsTestApp.swift
+//  CatsTest
+//
+//  Created by Rita Vasconcelos on 09/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CatsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
