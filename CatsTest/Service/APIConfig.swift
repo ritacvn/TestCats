70ba7f5a3dc94b4f9a54cf748e32f1e6473faf4d
ritacvn <ritadecassia.vasconcelos@hotmail.com>
2025-02-11 23:08:49 -0400

    Improvements to remove Url hardcoded

---
+++ CatsTest/Service/APIConfig.swift
@@ -0,0 +1,12 @@
+//
+//  APIConfig.swift
+//  CatsTest
+//
+//  Created by Rita Vasconcelos on 11/02/25.
+//
+
+import Foundation
+
+struct APIConfig {
+    static let baseURL = "https://api.thecatapi.com/v1"
+}
